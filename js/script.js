552ef45eae43113ff6c04b81793e8d975b25fb23
markcaf <marcocafassog@gmail.com>
2022-06-23 17:26:23 +0200

    Creating a function for the new square to be added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 // Dichiaro la variabile per il parent del DOM
 const gridContainer = document.getElementById('grid-container');
 console.log(gridContainer);
+
+// Creo una funzione per la creazione di un nuovo quadrato
+function createNewSquare (){
+    const currentSquare = document.createElement("div");
+    currentSquare.classList.add("square");
+    return currentSquare;
+}
